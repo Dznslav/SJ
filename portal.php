fe01617b0af2b6ab300951cab2ef5f15c137d108
Dzianis Laurentsyeu <146071428+Dznslav@users.noreply.github.com>
2024-04-02 21:42:46 +0200

    Add files via upload

--- portal.php
+++ portal.php
@@ -15,16 +15,14 @@
     <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Dzianis Laurentsyeu</a>
+        <a class="navbar-brand" href="index.html">DZNS Portal</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link" aria-current="page" href="index.html">Home</a>
-            <a class="nav-link" href="photos.html">Photography</a>
-            <a class="nav-link active" href="travels.html">Travels</a>
-            <a class="nav-link" href="portal.php">Portal</a>
+            <a class="nav-link" aria-current="page" href="index.html">Back to main page</a>
+            <a class="nav-link" href="">Login/Register</a>
           </div>
         </div>
       </div>
